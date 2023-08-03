f1d0f299e2633d3641937dfe29a16df1b700bbb5
Brian Maselli <bmaselli@Brians-M2-MacBook-Air.local>
2023-08-03 14:57:06 -0700

    Added some methods to CustomLists.cs

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -10,51 +10,108 @@ namespace CustomList
     {
         //Member Variables (HAS A)
         private T[] items;
-        private int capacity;
         private int count;
+        private int capacity;
+        private int Count { get { return count; } }
+        public int Capacity { get { return capacity; } set { capacity = value; } }
+
+        // Indexer returns or sets the corresponding item from the internal array 
+        public T this[int i]
+        {
+            get
+            {
+                return items[i];
+            }
+            set
+            {
+                items[i] = value;
+            }
+        }
 
         //Constructor
         public CustomList()
         {
-            //capacity = 4;
-            //count =
-            items = new T[4];
+            capacity = 4;
+            count = 0;
+            items = new T[capacity];
         }
 
-        public T this[int index]
-        {
-            get
-            {
-                return items[index];
-            }
-
-            set
-            {
-                items[index] = value;
-            }
-        }
 
         //Member Methods (CAN DO)
         public void Add(T item)
         {
-            //'item' parameter should be added to internal 'items' array
-            //if items array is at capacity, double capacity and create new array
-            //transfer all items to new array
+            if (count >= capacity)
+            {
+                capacity += 4;
+                T[] newArray = new T[capacity];
+
+                
+                for (int i = 0; i < count; i++)
+                {
+                    newArray[i] = items[i];
+                }
+
+                items = newArray;
+                items[count] = item;
+                count++;
+            }
+            else
+            {
+                items[count] = item;
+                count++;
+            }
         }
+
 
         public bool Remove(T item)
         {
-            //If 'item' exists in the 'items' array, remove its first instance
-            //Any items coming after the removed item should be shifted down so there is no empty index.
-            //If 'item' was removed, return true. If no item was removed, return false.
+            for (int i = 0; i < count; i++)
+            {
+                if (items[i].Equals(item))
+                {
+                    count--;
+                    ShiftMyArray(i);
+                    return true;
+                }
+            }
             return false;
         }
 
+
+        private void ShiftMyArray(int currentIndex)
+        {
+            for (int i = currentIndex; i < count; i++)
+            {
+                items[i] = items[i + 1];
+            }
+            items[count + 1] = default(T);
+        }
+
+
         public override string ToString()
         {
-            //returns a single string that contains all items from array
-            return "";
+            string resultString = "";
+            if (count != 0)
+                for (int i = 0; i < count; i++)
+                    resultString = ConvertValuesToString();
+
+            return resultString;
         }
+
+
+        private string ConvertValuesToString()
+        {
+            string myString = "";
+            for (int i = 0; i < count; i++)
+            {
+                myString += items[i];
+            }
+            return myString;
+
+
+        }
+
+
 
         public static CustomList<T> operator +(CustomList<T> firstList, CustomList<T> secondList)
         {
